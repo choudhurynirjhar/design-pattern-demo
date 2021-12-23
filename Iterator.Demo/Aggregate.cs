d195fc8344b1bc2c1dad97cd03a65bc6aa0f5a93
Nirjhar Choudhury <nirjhar_choudhury@trimble.com>
2021-12-22 22:20:27 -0500

    Add iterator pattern

---
+++ Iterator.Demo/Aggregate.cs
@@ -0,0 +1,32 @@
+﻿namespace Iterator.Demo;
+
+internal interface IAggregate<T>
+{
+    T this[int index] { get; set; }
+
+    int Count { get; }
+    IIterator<T> Iterator { get; }
+}
+
+internal class Aggregate<T> : IAggregate<T>
+{
+    private IIterator<T> _iterator;
+    private List<T> _list = new List<T>();
+
+    public T this[int index]
+    {
+        get { return _list[index]; }
+        set { _list.Add(value); }
+    }
+
+    public IIterator<T> Iterator
+    {
+        get
+        {
+            if (this._iterator == null) _iterator = new Iterator<T>(this);
+            return _iterator;
+        }
+    }
+
+    public int Count => _list.Count;
+}
---
+++ Iterator.Demo/Iterator.cs
@@ -0,0 +1,31 @@
+﻿namespace Iterator.Demo;
+
+internal interface IIterator<T>
+{
+    T Next();
+    T Current { get; }
+    bool IsLeft();
+}
+
+internal record Person(string Name, int Age);
+
+internal class Iterator<T> : IIterator<T>
+{
+    private readonly IAggregate<T> _aggregate;
+    int index = 0;
+
+    public Iterator(IAggregate<T> aggregate)
+    {
+        this._aggregate = aggregate;
+    }
+
+    public T Current => _aggregate[index];
+
+    public bool IsLeft() => index < _aggregate.Count;
+
+    public T Next()
+    {
+        index++;
+        return IsLeft() ? _aggregate[index] : default;
+    }
+}
